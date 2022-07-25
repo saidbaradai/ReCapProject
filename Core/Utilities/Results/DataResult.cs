5cc8aca0f5393015aad016b74569e3cf9a0de3a4
said baradai <saitbaradai@gmail.com>
2022-07-25 14:53:39 +0300

    added: Autofac

--- Core/Utilities/Results/DataResult.cs
+++ Core/Utilities/Results/DataResult.cs
@@ -12,11 +12,11 @@ namespace Core.Utilities.Results
 
         public DataResult(T data,bool isSuccess, string message):base(isSuccess,message)
         {
-
+            Data = data;
         }
         public DataResult(T data, bool isSuccess):base(isSuccess)
         {
-
+            Data = data;
         }
 
         public T Data { get; }
---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,44 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/Controller")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+
+        ICarService _carService;
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var cars = _carService.GetAll();
+
+            if (cars.IsSuccess)
+            {
+                return Ok(cars.Data);
+            }
+            return BadRequest(cars.Message);
+        }
+
+
+        [HttpPost("add")]
+        public IActionResult Add(Car car)
+        {
+            var cars = _carService.Add(car);
+            if (cars.IsSuccess)
+            {
+                return Ok(cars.Message);
+            }
+            return BadRequest(cars.Message);
+        }
+
+
+    }
+}
